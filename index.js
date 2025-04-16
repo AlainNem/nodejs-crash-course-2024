7025009885bc53ff360bba8fbeb29fb04fe1ad8a
AlainNem <KBCD4417@orange.com>
2025-04-16 15:44:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { getPosts } from "./postController.js";
+console.log(getPosts());
+/*const { generateRandomNumber, celciusToFahreinheit } = require('./utils');
+
+console.log(`Random number : ${generateRandomNumber()}`);
+console.log(`Celcius to fahreinheit : ${celciusToFahreinheit(32)}`);*/
